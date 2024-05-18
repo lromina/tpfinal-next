6891d4b429f8f41b62024f9a1ff039709e9b3092
lromina <lromina01@gmail.com>
2024-05-18 18:56:51 -0300

    intentando el inicio de sesion

--- next.config.js
+++ next.config.js
@@ -8,4 +8,13 @@ const nextConfig = {
 
 };
 
+
 module.exports = nextConfig;
+
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
+
